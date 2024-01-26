88772a3fcea3af0116a512d5495888bdc5e543db
Ariel Vollmann <vollmann.ariel+github@gmail.com>
2024-01-26 00:20:53 -0300

    simplificando

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <ESP8266WiFiMulti.h>
 #include <WebSocketsServer.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+//#include <ESP8266mDNS.h>
 #include <Hash.h>
 
 #define LED_RED     15
@@ -68,7 +68,7 @@ void setup() {
     USE_SERIAL.println();
     USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
+//    for(uint8_t t = 4; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
@@ -92,9 +92,9 @@ void setup() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
 
-    if(MDNS.begin("esp8266")) {
-        USE_SERIAL.println("MDNS responder started");
-    }
+//    if(MDNS.begin("esp8266")) {
+//        USE_SERIAL.println("MDNS responder started");
+//    }
 
     // handle index
     server.on("/", []() {
