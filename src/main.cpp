3b632706770139f209ef3b904bf2bb9eeb38e2ee
Ariel Vollmann <vollmann.ariel+github@gmail.com>
2024-01-26 01:43:29 -0300

    un poco más limpio

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,13 @@
-/*
- * WebSocketServerAllFunctionsDemo.ino
- *
- *  Created on: 10.05.2018
- *
- */
-
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 #include <WebSocketsServer.h>
 #include <ESP8266WebServer.h>
-//#include <ESP8266mDNS.h>
-#include <Hash.h>
 
 #define LED_RED     15
 #define LED_GREEN   12
 #define LED_BLUE    13
 
 #define USE_SERIAL Serial
-
-ESP8266WiFiMulti WiFiMulti;
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
@@ -59,59 +46,26 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 }
 
 void setup() {
-    //USE_SERIAL.begin(921600);
     USE_SERIAL.begin(115200);
-
-    //USE_SERIAL.setDebugOutput(true);
-
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-
-//    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
 
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GREEN, OUTPUT);
     pinMode(LED_BLUE, OUTPUT);
 
-    digitalWrite(LED_RED, 1);
-    digitalWrite(LED_GREEN, 1);
-    digitalWrite(LED_BLUE, 1);
+    WiFi.begin("TheLoudHouse", "romina260408");
 
-    WiFiMulti.addAP("TheLoudHouse", "romina260408");
-
-    while(WiFiMulti.run() != WL_CONNECTED) {
+    while(WiFi.status() != WL_CONNECTED) {
         delay(100);
     }
 
-    // start webSocket server
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
 
-//    if(MDNS.begin("esp8266")) {
-//        USE_SERIAL.println("MDNS responder started");
-//    }
-
-    // handle index
     server.on("/", []() {
-        // send index.html
         server.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>");
     });
 
     server.begin();
-
-    // Add service to MDNS
-//    MDNS.addService("http", "tcp", 80);
-//    MDNS.addService("ws", "tcp", 81);
-
-    digitalWrite(LED_RED, 0);
-    digitalWrite(LED_GREEN, 0);
-    digitalWrite(LED_BLUE, 0);
-
 }
 
 unsigned long last_10sec = 0;
